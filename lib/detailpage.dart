1074ac27b27cf043accb89a960155c19ed69ee32
Rebonanza <giventheo2211@gmail.com>
2023-03-21 09:45:57 +0700

    feat: add home page and detail page

---
+++ lib/detailpage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:identity_detai_flutter/identity.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DetailPage extends StatefulWidget {
+  final Identity identitasTerima;
+  const DetailPage({Key? key, required this.identitasTerima}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Detail Page"),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Text("Ini Detail Page ${widget.identitasTerima.name}"),
+                Text(widget.identitasTerima.username),
+                Image.network(widget.identitasTerima.fotoUrls),
+                Text("Email : "),
+                Text(widget.identitasTerima.email[0]),
+                Text(widget.identitasTerima.email[1]),
+                Text("Company : "),
+                Text(widget.identitasTerima.company[0][0]),
+                Text(widget.identitasTerima.company[0][1]),
+                Text(widget.identitasTerima.company[1][0]),
+                Text(widget.identitasTerima.company[1][1]),
+                // OutlinedButton(
+                //   child: Text('Kembali'),
+                //   onPressed: () {
+                //     Navigator.pop(context);
+                //   },
+                // ),
+                // ElevatedButton(
+                //   child: Text('Pindah Screen tanpa Stack'),
+                //   onPressed: () {
+                //     Navigator.pushReplacement(context,
+                //         MaterialPageRoute(builder: (context) {
+                //       return HomePage();
+                //     }));
+                //   },
+                // ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    _launchURL('https://google.com');
+                  },
+                  child: Text("google"),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:identity_detai_flutter/detailpage.dart';
+import 'package:identity_detai_flutter/identity.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Main Page"),
+          ),
+          body: ListView.builder(
+            itemBuilder: (context, index) {
+              final dataId = listIdentity[index];
+              return Card(
+                  child: InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DetailPage(
+                      identitasTerima: dataId,
+                    );
+                  }));
+                },
+                child: Column(
+                  children: [
+                    Image.network(dataId.fotoUrls),
+                    Text(dataId.name),
+                  ],
+                ),
+              ));
+            },
+            itemCount: listIdentity.length,
+          )),
+    );
+  }
+}
